339f7bad940925cb17eb519ab0a0ee91cacfdc04
student <you@example.com>
2015-11-10 12:09:53 +0300

    ff

--- ex1.py
+++ ex1.py
@@ -1,7 +1,8 @@
 class Point:
-    def __init__(self, x=0, y=0):
-        self.x=x
-        self.y=y
+    def __init__(self, s='0,0'):
+        coord=list(map(float, s.split()))
+        self.x=coord[0]
+        self.y=coord[1]
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
     def __lt__(self, other):
@@ -11,10 +12,23 @@ class Point:
     def __mul__(self,other):
         return Point(self.x * other.x, self.y + other.y)
 N=int(input())
+xm=0
+ym=0
+rmax=0
+cmx=0
+cmy=0
 for i in range(0,N):
-    x=int(input())
-    y=int(input())
-    a=Point(x,y)
-    a = input().split()
-    c = x + y
-print(c)    
+    for k in range(i+1,N):
+        for j in range(i+2,N):
+            P=((A[i]-A[j])**2 + (A[j]-A[k])**2 + (A[k]-A[i])**2 + 
+    s=input()
+    a=Point(s)
+    rm=((a.x)**2 + (a.y)**2)**0.5 
+    cmx=cmx + a.x
+    cmy=cmy + a.y
+    if 
+    if rm>rmax:
+        rmax=rm
+        xm=a.x
+        ym=a.y       
+print(xm,ym,cmx/N,cmy/N)  
